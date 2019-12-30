4e22dc342a2c311308e70fff943b063d3fef89d2
Timofei1 <57501337+Timofei1@users.noreply.github.com>
2019-12-30 22:45:49 +0400

    Updates

---
+++ precache-manifest.ea3e176b064d1f77f68615c0d02f3954.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbf37ea56fb2190d870ec3ba9e672708",
+    "url": "/kvant/index.html"
+  },
+  {
+    "revision": "934721de31cd4f3d81e7",
+    "url": "/kvant/static/css/2.4e89ce74.chunk.css"
+  },
+  {
+    "revision": "7295aa90f4d3bff48c6b",
+    "url": "/kvant/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "934721de31cd4f3d81e7",
+    "url": "/kvant/static/js/2.e36e3b37.chunk.js"
+  },
+  {
+    "revision": "7295aa90f4d3bff48c6b",
+    "url": "/kvant/static/js/main.05c235dd.chunk.js"
+  },
+  {
+    "revision": "ca8aa232f91a55575424",
+    "url": "/kvant/static/js/runtime-main.3ad27b7e.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/kvant/static/media/persik.4e1ec840.png"
+  }
+]);
