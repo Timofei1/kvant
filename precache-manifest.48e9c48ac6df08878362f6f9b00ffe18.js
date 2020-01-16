34ea1573d312496f9f1c47b257ec542547f6ebe3
student <student@obr18.ru>
2020-01-16 11:03:57 +0400

    Updates

---
+++ precache-manifest.48e9c48ac6df08878362f6f9b00ffe18.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcfb80be482ac389096042b53f399bfd",
+    "url": "/kvant/index.html"
+  },
+  {
+    "revision": "3eb19fde3d95c1961b40",
+    "url": "/kvant/static/css/2.2cfa1b23.chunk.css"
+  },
+  {
+    "revision": "3eb19fde3d95c1961b40",
+    "url": "/kvant/static/js/2.54ea6bba.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/kvant/static/js/2.54ea6bba.chunk.js.LICENSE"
+  },
+  {
+    "revision": "afb145bbfd8533930dd5",
+    "url": "/kvant/static/js/main.ba86843a.chunk.js"
+  },
+  {
+    "revision": "895b1d6de2bed57fc08c",
+    "url": "/kvant/static/js/runtime-main.5c53b31f.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/kvant/precache-manifest.1041040905fa8c51eb87458523fa0602.js"
+  "/kvant/precache-manifest.48e9c48ac6df08878362f6f9b00ffe18.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/kvant/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
